ce9ea160ed0d469705ab1be8bd5299eff35315ea
Paulo Soares <paulosoares@SBS-7186.local>
2022-07-16 13:08:22 -0700

    Discrete inference with synthetic data for ASIST report

--- src/inference/vocalics.py
+++ src/inference/vocalics.py
@@ -1,13 +1,174 @@
-from scipy.stats import bernoulli
-from typing import Any, Callable, Dict, List
+from typing import Any
+from typing import Callable
+from typing import Dict
+from typing import List
+from typing import Optional
+
 import numpy as np
 from tqdm import tqdm
+
+from scipy.stats import bernoulli
 from scipy.stats import norm, truncnorm
 
 EPSILON = 1E-16
 
 
-def estimate_discrete_coordination(series_a: np.ndarray, series_b: np.ndarray, prior_p: float,
+class DiscreteCoordinationInference:
+
+    def __init__(self, series_a: np.ndarray, series_b: np.ndarray, prior_c: float,
+                 pc: float, mean_a: np.array, std_a: np.array, mean_b: np.ndarray, std_b: np.ndarray,
+                 std_ab: np.ndarray, mask_a: Optional[np.ndarray], mask_b: Optional[np.ndarray]):
+        """
+        This class estimates discrete coordination with message passing.
+        The series A and B can contain  multiple features (n), e.g. picth and intensity.
+
+        @param series_a: n x T vector with average vocalics for process A.
+        @param series_b: n x T vector with average vocalics for process B.
+        @param prior_c: prior distribution of coordination, i.e., distribution of coordination at time t = 0
+        @param pc: probability that coordination does not change from time t to time t+1
+        @param mean_a: mean of the prior of series A when there's no coordination
+        @param std_a: standard deviation of series A when there's no coordination
+        @param mean_b: mean of the prior of series B when there's no coordination
+        @param std_b: standard deviation of series B when there's no coordination
+        @param std_ab: standard deviation of series A and B when there's coordination
+        @param mask_a: binary array indicating the time steps when there are values for series A
+        @param mask_b: binary array indicating the time steps when there are values for series B
+        @return:
+        """
+
+        self.series_a = series_a
+        self.series_b = series_b
+        self.prior_c = prior_c
+        self.pc = pc
+        self.mean_a = mean_a
+        self.std_a = std_a
+        self.mean_b = mean_b
+        self.std_b = std_b
+        self.std_ab = std_ab
+
+        self.num_features, self.time_steps = series_a.shape  # n and T
+        self.mid_time_step = int(self.time_steps / 2)  # M
+
+        self.mask_a = np.ones(self.time_steps) if mask_a is None else mask_a
+        self.mask_b = np.ones(self.time_steps) if mask_b is None else mask_b
+
+        # C_{t-1} to C_t and vice-versa since the matrix is symmetric
+        self.transition_matrix = np.array([[pc, 1 - pc], [1 - pc, pc]])
+
+    def estimate_marginals(self):
+        m_comp2coord = self.__get_messages_from_components_to_coordination()
+        m_forward = self.__forward(m_comp2coord)
+        m_backwards = self.__backwards(m_comp2coord)
+
+        m_backwards = np.roll(m_backwards, shift=-1, axis=0)
+        m_backwards[-1] = 1
+        # alpha(C_t) * beta(C_{t+1}) x Transition Matrix
+        c_marginals = m_forward * np.matmul(m_backwards, self.transition_matrix)
+        c_marginals /= np.sum(c_marginals, axis=1, keepdims=True)
+
+        return c_marginals
+
+    def __forward(self, m_comp2coord: np.ndarray):
+        m_forward = np.zeros((self.mid_time_step + 1, 2))
+        for t in range(self.mid_time_step + 1):
+            # Contribution of the previous coordination sample to the marginal
+            if t == 0:
+                m_forward[t] = np.array([1 - self.prior_c, self.prior_c], dtype=float)
+            else:
+                m_forward[t] = np.matmul(m_forward[t - 1], self.transition_matrix)
+
+            # Contribution of the components to the coordination marginal
+            if t == self.mid_time_step:
+                # All the components contributions after t = M
+                # TODO - This can result in a very small number. Check if we can work on log scale.
+                m_forward[t] *= np.prod(m_comp2coord[t:], axis=0)
+            else:
+                m_forward[t] *= m_comp2coord[t]
+            m_forward[t] /= np.sum(m_forward[t])  # Message normalization
+
+        return m_forward
+
+    def __backwards(self, m_comp2coord: np.ndarray):
+        m_backwards = np.zeros((self.mid_time_step + 1, 2))
+        for t in range(self.mid_time_step, -1, -1):
+            # Contribution of the next coordination sample to the marginal
+            if t == self.mid_time_step:
+                # All the components contributions after t = M
+                # TODO - This can result in a very small number. Check if we can work on log scale.
+                m_backwards[t] = np.prod(m_comp2coord[t:], axis=0)
+            else:
+                m_backwards[t] = np.matmul(m_backwards[t + 1], self.transition_matrix)
+                m_backwards[t] *= m_comp2coord[t]
+
+            m_backwards[t] /= np.sum(m_backwards[t])  # Message normalization
+
+        return m_backwards
+
+    def __get_messages_from_components_to_coordination(self):
+        def get_message_from_individual_component_to_coordination(current_value_main_series: np.ndarray,
+                                                                  previous_value_main_series: np.ndarray,
+                                                                  previous_value_other_series: np.ndarray,
+                                                                  prior_mean_main_series: np.ndarray,
+                                                                  prior_std_main_series: np.ndarray,
+                                                                  coupling_std: np.ndarray,
+                                                                  mask_main_series: int):
+            # This term will be 0.5 only if there are no observations for the component at the current time step.
+            # We use it so that c_leaves = [0.5, 0.5] instead of [0, 0] in these cases for numerical stability
+            # with vector operations later when passing the messages around.
+            addition_factor = (1 - mask_main_series) * 0.5
+            prior_cdf = np.prod(
+                norm.pdf(current_value_main_series, loc=prior_mean_main_series, scale=prior_std_main_series))
+
+            if previous_value_other_series is None:
+                # Nothing can be inferred about coordination
+                c0 = 0.5
+                c1 = 0.5
+            else:
+                # For C_t = 0
+                c0 = addition_factor + mask_main_series * (prior_cdf if previous_value_main_series is None else np.prod(
+                    norm.pdf(
+                        current_value_main_series, loc=previous_value_main_series, scale=prior_std_main_series)))
+
+                # For C_t = 1
+                c1 = addition_factor + mask_main_series * np.prod(
+                    norm.pdf(current_value_main_series, loc=previous_value_other_series, scale=coupling_std))
+
+            return np.array([c0, c1])
+
+        last_ta = None  # last time step with observed value for the series A
+        last_tb = None  # last time step with observed value for the series B
+        m_comp2coord = np.zeros((self.time_steps, 2))
+        for t in range(self.time_steps):
+            previous_a = None if last_ta is None else self.series_a[:, last_ta]
+            previous_b = None if last_tb is None else self.series_b[:, last_tb]
+
+            # Message from A_t to C_t
+            m_comp2coord[t] = get_message_from_individual_component_to_coordination(self.series_a[:, t],
+                                                                                    previous_a,
+                                                                                    previous_b,
+                                                                                    self.mean_a,
+                                                                                    self.std_a,
+                                                                                    self.std_ab,
+                                                                                    self.mask_a[t])
+
+            # Message from B_t to C_t
+            m_comp2coord[t] *= get_message_from_individual_component_to_coordination(self.series_b[:, t],
+                                                                                     previous_b,
+                                                                                     previous_a,
+                                                                                     self.mean_b,
+                                                                                     self.std_b,
+                                                                                     self.std_ab,
+                                                                                     self.mask_b[t])
+
+            if self.mask_a[t] == 1:
+                last_ta = t
+            if self.mask_b[t] == 1:
+                last_tb = t
+
+        return m_comp2coord
+
+
+def estimate_discrete_coordination(series_a: np.ndarray, series_b: np.ndarray, prior_c: float,
                                    transition_p: float, pa: Callable, pb: Callable, mask_a: Any,
                                    mask_b: Any) -> np.ndarray:
     """
@@ -34,7 +195,7 @@ def estimate_discrete_coordination(series_a: np.ndarray, series_b: np.ndarray, p
     for t in range(T):
         # Contribution of the previous coordination sample to the marginal
         if t == 0:
-            c_forward[t] = np.array([1 - prior_p, prior_p], dtype=float)
+            c_forward[t] = np.array([1 - prior_c, prior_c], dtype=float)
         else:
             c_forward[t] = np.matmul(c_forward[t - 1], transition_matrix)
 
