d783726eca1c519c7d849f35b3b6943cc88258b9
Paulo Soares <paulosoares@SBS-7186.local>
2022-10-13 10:48:00 -0700

    Refactoring discrete model to use sklearn interface

--- coordination/inference/discrete_coordination.py
+++ coordination/inference/discrete_coordination.py
@@ -1,9 +1,9 @@
-from typing import Callable, Tuple
+from typing import Callable, List
 
 import numpy as np
 from scipy.stats import norm
 
-from coordination.component.speech.common import VocalicsSparseSeries
+from coordination.common.dataset import Dataset, SeriesData
 from coordination.inference.inference_engine import InferenceEngine
 
 EPSILON = 1E-16
@@ -11,15 +11,18 @@ EPSILON = 1E-16
 
 class DiscreteCoordinationInferenceFromVocalics(InferenceEngine):
 
-    def __init__(self, vocalic_series: VocalicsSparseSeries, p_prior_coordination: float,
-                 p_coordination_transition: float, mean_prior_vocalics: np.array, std_prior_vocalics: np.array,
-                 std_uncoordinated_vocalics: np.ndarray, std_coordinated_vocalics: np.ndarray,
-                 f: Callable = lambda x, s: x, fix_coordination_on_second_half: bool = True):
+    def __init__(self,
+                 p_prior_coordination: float,
+                 p_coordination_transition: float,
+                 mean_prior_vocalics: np.array,
+                 std_prior_vocalics: np.array,
+                 std_uncoordinated_vocalics: np.ndarray,
+                 std_coordinated_vocalics: np.ndarray,
+                 f: Callable = lambda x, s: x,
+                 fix_coordination_on_second_half: bool = True):
         """
         This class estimates discrete coordination with message passing.
-        The series A and B can contain multiple series, one for each feature (e.g. pitch and intensity).
 
-        @param vocalic_series: contains n x T vector with average vocalics for subject.
         @param p_prior_coordination: probability of coordination at the initial timestep.
         @param p_coordination_transition: probability that coordination changes from time t to time t+1.
         @param mean_prior_vocalics: mean of the distribution of vocalics at the first time it is observed.
@@ -29,12 +32,6 @@ class DiscreteCoordinationInferenceFromVocalics(InferenceEngine):
         @param fix_coordination_on_second_half: whether coordination in the second half of the mission should be fixed.
         """
 
-        assert len(mean_prior_vocalics) == vocalic_series.num_series
-        assert len(std_prior_vocalics) == vocalic_series.num_series
-        assert len(std_uncoordinated_vocalics) == vocalic_series.num_series
-        assert len(std_coordinated_vocalics) == vocalic_series.num_series
-
-        self._vocalic_series = vocalic_series
         self._p_prior_coordination = p_prior_coordination
         self._mean_prior_vocalics = mean_prior_vocalics
         self._std_prior_vocalics = std_prior_vocalics
@@ -43,32 +40,48 @@ class DiscreteCoordinationInferenceFromVocalics(InferenceEngine):
         self._f = f
         self._fix_coordination_on_second_half = fix_coordination_on_second_half
 
-        self._num_features, self._time_steps = vocalic_series.values.shape  # n and T
-
         # C_{t-1} to C_t and vice-versa since the matrix is symmetric
         self._prior_vector = np.array([1 - p_prior_coordination, p_prior_coordination])
         self._transition_matrix = np.array([
             [1 - p_coordination_transition, p_coordination_transition],
             [p_coordination_transition, 1 - p_coordination_transition]])
 
-    def estimate_means_and_variances(self) -> np.ndarray:
-        m_comp2coord = self._get_messages_from_components_to_coordination()
-        m_forward = self._forward(m_comp2coord)
-        m_backwards = self._backwards(m_comp2coord)
+    def fit(self, input_features: Dataset, num_particles: int = 0, num_iter: int = 0, discard_first: int = 0, *args,
+            **kwargs):
+        # MCMC to train parameters? We start by choosing with cross validation instead.
+        raise NotImplementedError
 
-        m_backwards = np.roll(m_backwards, shift=-1, axis=1)
-        m_backwards[:, -1] = 1
+    def predict(self, input_features: Dataset, num_particles: int = 0, *args, **kwargs) -> List[np.ndarray]:
+        if input_features.num_trials > 0:
+            assert len(self._mean_prior_vocalics) == input_features.series[0].vocalics.num_series
+            assert len(self._std_prior_vocalics) == input_features.series[0].vocalics.num_series
+            assert len(self._std_uncoordinated_vocalics) == input_features.series[0].vocalics.num_series
+            assert len(self._std_coordinated_vocalics) == input_features.series[0].vocalics.num_series
 
-        # alpha(C_t) * beta(C_{t+1}) x Transition Matrix
-        c_marginals = m_forward * np.matmul(m_backwards.T, self._transition_matrix.T).T
-        c_marginals /= np.sum(c_marginals, axis=0, keepdims=True)
+        result = []
+        for d in range(input_features.num_trials):
+            series = input_features.series[d]
 
-        # There's no variance in exact estimation for discrete state-space
-        return np.vstack([c_marginals[1], np.zeros_like(c_marginals[1])])
+            m_comp2coord = self._get_messages_from_components_to_coordination(series)
+            m_forward = self._forward(m_comp2coord)
+            m_backwards = self._backwards(m_comp2coord)
 
-    def _forward(self, m_comp2coord: np.ndarray) -> np.ndarray:
-        M = int(self._time_steps / 2)
-        num_time_steps = M + 1 if self._fix_coordination_on_second_half else self._time_steps
+            m_backwards = np.roll(m_backwards, shift=-1, axis=1)
+            m_backwards[:, -1] = 1
+
+            # alpha(C_t) * beta(C_{t+1}) x Transition Matrix
+            c_marginals = m_forward * np.matmul(m_backwards.T, self._transition_matrix.T).T
+            c_marginals /= np.sum(c_marginals, axis=0, keepdims=True)
+
+            # There's no variance in exact estimation for discrete state-space
+            params = np.vstack([c_marginals[1], np.zeros_like(c_marginals[1])])
+            result.append(params)
+
+        return result
+
+    def _forward(self, m_comp2coord: np.ndarray, series: SeriesData) -> np.ndarray:
+        M = int(series.num_time_steps / 2)
+        num_time_steps = M + 1 if self._fix_coordination_on_second_half else series.num_time_steps
 
         m_forward = np.zeros((2, num_time_steps))
         for t in range(num_time_steps):
@@ -94,9 +107,9 @@ class DiscreteCoordinationInferenceFromVocalics(InferenceEngine):
 
         return m_forward
 
-    def _backwards(self, m_comp2coord: np.ndarray) -> np.ndarray:
-        M = int(self._time_steps / 2)
-        num_time_steps = M + 1 if self._fix_coordination_on_second_half else self._time_steps
+    def _backwards(self, m_comp2coord: np.ndarray, series: SeriesData) -> np.ndarray:
+        M = int(series.num_time_steps / 2)
+        num_time_steps = M + 1 if self._fix_coordination_on_second_half else series.num_time_steps
 
         m_backwards = np.zeros((2, num_time_steps))
         for t in range(num_time_steps - 1, -1, -1):
@@ -118,23 +131,23 @@ class DiscreteCoordinationInferenceFromVocalics(InferenceEngine):
 
         return m_backwards
 
-    def _get_messages_from_components_to_coordination(self) -> np.ndarray:
+    def _get_messages_from_components_to_coordination(self, series: SeriesData) -> np.ndarray:
         prior = norm(loc=self._mean_prior_vocalics, scale=self._std_prior_vocalics)
 
-        m_comp2coord = np.zeros((2, self._time_steps))
-        for t in range(self._time_steps):
-            if self._vocalic_series.mask[t] == 0:
+        m_comp2coord = np.zeros((2, series.num_time_steps))
+        for t in range(series.num_time_steps):
+            if series.vocalics.mask[t] == 0:
                 # We cannot tell anything about coordination if there's no observation
                 m_comp2coord[:, t] = np.array([0.5, 0.5])
                 continue
 
             # A represents the current vocalic value and the previous vocalic value from the same speaker.
             # B carries the most recent vocalic value from a different speaker than A.
-            A_t = self._f(self._vocalic_series.values[:, t], 0)
-            A_prev = None if self._vocalic_series.previous_from_self[t] is None else self._f(
-                self._vocalic_series.values[:, self._vocalic_series.previous_from_self[t]], 0)
-            B_prev = None if self._vocalic_series.previous_from_other[t] is None else self._f(
-                self._vocalic_series.values[:, self._vocalic_series.previous_from_other[t]], 1)
+            A_t = self._f(series.vocalics.values[:, t], 0)
+            A_prev = None if series.vocalics.previous_from_self[t] is None else self._f(
+                series.vocalics.values[:, series.vocalics.previous_from_self[t]], 0)
+            B_prev = None if series.vocalics.previous_from_other[t] is None else self._f(
+                series.vocalics.values[:, series.vocalics.previous_from_other[t]], 1)
 
             if B_prev is None:
                 # Nothing can be inferred about coordination if there's no previous observation from another subject
