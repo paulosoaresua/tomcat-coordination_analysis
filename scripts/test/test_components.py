9959cdf47daf57ce6c79be2093923efa0f9eebbd
Paulo Soares <paulosoares@email.arizona.edu>
2023-02-06 11:28:52 -0300

    Inference in the component's scale. (#47)
    Co-authored-by: Paulo Soares <paulosoares@SBS-7186.local>

--- scripts/test/test_components.py
+++ scripts/test/test_components.py
@@ -8,10 +8,10 @@ from coordination.model.components.mixture_component import MixtureComponent, mi
 from coordination.model.components.observation_component import ObservationComponent
 
 if __name__ == "__main__":
-    T = 200
+    T = 400
     F = 2
 
-    mix = MixtureComponent("mix", 3, F, True, sd_mean_a0=np.ones((3, F)), sd_sd_aa=np.full((3, F), fill_value=2),
+    mix = MixtureComponent("mix", 3, F, False, sd_mean_a0=np.ones((3, F)), sd_sd_aa=np.full((3, F), fill_value=2),
                            a_mixture_weights=np.ones((3, 2)))
     obs = ObservationComponent("obs", 3, F, sd_sd_o=np.ones((3, F)))
 
@@ -20,12 +20,12 @@ if __name__ == "__main__":
     mix.parameters.mixture_weights.value = np.array([[0.3, 0.7], [0.8, 0.2], [0.1, 0.9]])
     obs.parameters.sd_o.value = np.ones((3, F))
 
-    coordination_values = np.ones(shape=(1, T))
-    mix_samples = mix.draw_samples(num_series=1, num_time_steps=T, seed=0, relative_frequency=1,
-                                   coordination=coordination_values)
+    np.random.seed(0)
+    coordination_values = np.random.rand(1, T)
+    # coordination_values = np.zeros(shape=(1, T))
+    mix_samples = mix.draw_samples(num_series=1, seed=0, relative_frequency=2, coordination=coordination_values)
     obs_samples = obs.draw_samples(seed=None,
-                                   latent_component=mix_samples.values,
-                                   latent_mask=mix_samples.mask)
+                                   latent_component=mix_samples.values)
 
     # sample = mixture_random_with_self_dependency(
     #     initial_mean=mix.parameters.mean_a0,
@@ -45,22 +45,22 @@ if __name__ == "__main__":
     # print(sample)
 
     plt.figure()
-    plt.plot(np.arange(T), mix_samples.values[0, 0, 0], marker="o", color="tab:blue", markersize=10)
-    plt.plot(np.arange(T), obs_samples.values[0, 0, 0], marker="o", color="tab:pink", markersize=5)
+    plt.plot(np.arange(mix_samples.num_time_steps), mix_samples.values[0, 0, 0], marker="o", color="tab:blue",
+             markersize=10)
+    plt.plot(np.arange(mix_samples.num_time_steps), obs_samples.values[0, 0, 0], marker="o", color="tab:pink",
+             markersize=5)
     plt.title(f"Samples")
     plt.show()
 
     mix.parameters.clear_values()
     obs.parameters.clear_values()
-    prev_time_mask = np.where(mix_samples.prev_time >= 0, 1, 0)
-    with pm.Model(coords={"sub": np.arange(3), "time": np.arange(T), "fea": np.arange(F)}) as model:
-        latent_component = mix.update_pymc_model(ptt.constant(coordination_values[0]),
-                                                 ptt.constant(mix_samples.prev_time[0]),
-                                                 ptt.constant(np.where(mix_samples.prev_time[0] >= 0, 1, 0)),
-                                                 ptt.constant(mix_samples.mask[0]),
-                                                 subject_dimension="sub",
-                                                 time_dimension="time",
-                                                 feature_dimension="fea")
+    with pm.Model(
+            coords={"sub": np.arange(3), "time": np.arange(mix_samples.num_time_steps), "fea": np.arange(F)}) as model:
+        latent_component = mix.update_pymc_model(
+            ptt.constant(coordination_values[0, mix_samples.time_steps_in_coordination_scale[0]]),
+            subject_dimension="sub",
+            time_dimension="time",
+            feature_dimension="fea")
         obs.update_pymc_model(latent_component, obs_samples.values[0])
 
         idata = pm.sample_prior_predictive(random_seed=0)
@@ -68,8 +68,9 @@ if __name__ == "__main__":
         prior_obs = idata.prior_predictive["obs"].sel(chain=0).to_numpy()
 
         plt.figure()
-        plt.plot(np.arange(T)[:, None].repeat(500, axis=1), prior_obs[:, 0, 0].T, color="tab:blue", alpha=0.3)
-        plt.plot(np.arange(T), obs_samples.values[0, 0, 0], marker="o", color="black")
+        plt.plot(np.arange(mix_samples.num_time_steps)[:, None].repeat(500, axis=1), prior_obs[:, 0, 0].T,
+                 color="tab:blue", alpha=0.3)
+        plt.plot(np.arange(mix_samples.num_time_steps), obs_samples.values[0, 0, 0], marker="o", color="black")
         plt.title(f"Prior Predictive Obs")
         plt.show()
 
@@ -81,7 +82,9 @@ if __name__ == "__main__":
         posterior = idata.posterior["mix"].sel(chain=0).to_numpy()
 
         plt.figure()
-        plt.plot(np.arange(T)[:, None].repeat(1000, axis=1), posterior[:, 0, 0].T, color="tab:blue", alpha=0.3)
-        plt.plot(np.arange(T), mix_samples.values[0, 0, 0], marker="o", color="black", alpha=0.7)
+        plt.plot(np.arange(mix_samples.num_time_steps)[:, None].repeat(1000, axis=1), posterior[:, 0, 0].T,
+                 color="tab:blue", alpha=0.3)
+        plt.plot(np.arange(mix_samples.num_time_steps), mix_samples.values[0, 0, 0], marker="o", color="black",
+                 alpha=0.7)
         plt.title(f"Posterior Predictive")
         plt.show()
