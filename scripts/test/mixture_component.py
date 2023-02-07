73804efa9900e8dda0f82a5e18a40fe1784638ab
Paulo Soares <paulosoares@SBS-7186.local>
2023-02-07 15:38:31 -0300

    Serialized component done.

--- scripts/test/mixture_component.py
+++ scripts/test/mixture_component.py
@@ -1,38 +1,38 @@
 import pymc as pm
-import pytensor.tensor as ptt
 import numpy as np
 import matplotlib.pyplot as plt
 import arviz as az
 
 from coordination.model.components.coordination_component import SigmoidGaussianCoordinationComponent
-from coordination.model.components.mixture_component import MixtureComponent, mixture_random_with_self_dependency
+from coordination.model.components.mixture_component import MixtureComponent
 from coordination.model.components.observation_component import ObservationComponent
 
 from coordination.common.functions import sigmoid
 
 if __name__ == "__main__":
-    T = 1024
+    T = 100
     F = 2
-    FREQ = 4
+    FREQ = 1
 
-    coord = SigmoidGaussianCoordinationComponent(0.5, 1, 0.01)
-    mix = MixtureComponent("mix", 3, F, False, sd_mean_a0=np.ones((3, F)), sd_sd_aa=np.full((3, F), fill_value=2),
-                           a_mixture_weights=np.ones((3, 2)))
-    obs = ObservationComponent("obs", 3, F, sd_sd_o=np.ones((3, F)))
+    # Components
+    coordination_cpn = SigmoidGaussianCoordinationComponent(initial_coordination=0.5, sd_uc=1)
+    mixture_cpn = MixtureComponent("mix", 3, F, True, sd_mean_a0=np.ones((3, F)),
+                                   sd_sd_aa=np.full((3, F), fill_value=2),
+                                   a_mixture_weights=np.ones((3, 2)))
+    observation_cpn = ObservationComponent("obs", 3, F, sd_sd_o=np.ones((3, F)))
 
-    coord.parameters.sd_uc.value = np.full(1, fill_value=0.1)
-    mix.parameters.mean_a0.value = np.zeros((3, F))
-    mix.parameters.sd_aa.value = np.ones((3, F))
-    mix.parameters.mixture_weights.value = np.array([[0.3, 0.7], [0.8, 0.2], [0.1, 0.9]])
-    obs.parameters.sd_o.value = np.ones((3, F))
+    # Parameters
+    coordination_cpn.parameters.sd_uc.value = np.full(1, fill_value=0.1)
+    mixture_cpn.parameters.mean_a0.value = np.zeros((3, F))
+    mixture_cpn.parameters.sd_aa.value = np.ones((3, F))
+    mixture_cpn.parameters.mixture_weights.value = np.array([[0.3, 0.7], [0.8, 0.2], [0.1, 0.9]])
+    observation_cpn.parameters.sd_o.value = np.ones((3, F))
 
-    np.random.seed(0)
-    # coordination_values = np.random.rand(1, T)
-    # coordination_values = np.zeros(shape=(1, T))
-    coordination_values = coord.draw_samples(1, T, 0).coordination
-    mix_samples = mix.draw_samples(num_series=1, seed=None, relative_frequency=FREQ, coordination=coordination_values)
-    obs_samples = obs.draw_samples(seed=None,
-                                   latent_component=mix_samples.values)
+    # Samples
+    coordination_values = np.ones((1, T))  # coordination_cpn.draw_samples(num_series=1, num_time_steps=T, seed=0).coordination
+    mix_samples = mixture_cpn.draw_samples(num_series=1, seed=None, relative_frequency=FREQ,
+                                           coordination=coordination_values)
+    obs_samples = observation_cpn.draw_samples(seed=None, latent_component=mix_samples.values)
 
     # sample = mixture_random_with_self_dependency(
     #     initial_mean=mix.parameters.mean_a0,
@@ -51,35 +51,37 @@ if __name__ == "__main__":
     # )
     # print(sample)
 
+    # Plot feature
     plt.figure()
     plt.plot(np.arange(mix_samples.num_time_steps), mix_samples.values[0, 0, 0], marker="o", color="tab:blue",
              markersize=10)
     plt.plot(np.arange(mix_samples.num_time_steps), obs_samples.values[0, 0, 0], marker="o", color="tab:pink",
              markersize=5)
-    plt.title(f"Samples Component")
+    plt.title(f"Samples - 1st Feature from 1st Individual")
     plt.show()
 
+    # Plot coordination
     plt.figure()
-    plt.plot(np.arange(T), coordination_values[0], marker="o", color="tab:blue",
-             markersize=10)
+    plt.plot(np.arange(T), coordination_values[0], marker="o", color="tab:blue", markersize=5)
     plt.title(f"Samples Coordination")
     plt.show()
 
-    mix.parameters.clear_values()
-    obs.parameters.clear_values()
-    coord.parameters.clear_values()
+    # Joint inference
+    mixture_cpn.parameters.clear_values()
+    observation_cpn.parameters.clear_values()
+    coordination_cpn.parameters.clear_values()
     with pm.Model(
             coords={"sub": np.arange(3), "coord_time": np.arange(T),
                     "cpn_time": np.arange(mix_samples.num_time_steps), "fea": np.arange(F)}) as model:
-        _, coordination = coord.update_pymc_model("coord_time")
-        latent_component = mix.update_pymc_model(
-            # ptt.constant(coordination[mix_samples.time_steps_in_coordination_scale[0]]),
+        _, coordination = coordination_cpn.update_pymc_model("coord_time")
+        latent_component = mixture_cpn.update_pymc_model(
             coordination[mix_samples.time_steps_in_coordination_scale[0]],
             subject_dimension="sub",
             time_dimension="cpn_time",
             feature_dimension="fea")
-        obs.update_pymc_model(latent_component, obs_samples.values[0])
+        observation_cpn.update_pymc_model(latent_component, obs_samples.values[0])
 
+        # Predictive prior
         idata = pm.sample_prior_predictive(random_seed=0)
 
         prior_obs = idata.prior_predictive["obs"].sel(chain=0).to_numpy()
@@ -87,35 +89,36 @@ if __name__ == "__main__":
         plt.figure()
         plt.plot(np.arange(mix_samples.num_time_steps)[:, None].repeat(500, axis=1), prior_obs[:, 0, 0].T,
                  color="tab:blue", alpha=0.3)
-        plt.plot(np.arange(mix_samples.num_time_steps), obs_samples.values[0, 0, 0], marker="o", color="black")
+        plt.plot(np.arange(mix_samples.num_time_steps), obs_samples.values[0, 0, 0], marker="o", color="black",
+                 markersize=5)
         plt.title(f"Prior Predictive Obs")
         plt.show()
 
+        # Posterior
         idata = pm.sample(draws=1000, tune=1000, chains=2, init="jitter+adapt_diag", random_seed=0)
 
+        #   Parameters
         az.plot_trace(idata, var_names=["mixture_weights_mix", "mean_a0_mix", "sd_aa_mix", "sd_o_obs", "sd_uc"])
         plt.show()
 
+        #   Latent feature
         posterior = idata.posterior["mix"].sel(chain=0).to_numpy()
 
         plt.figure()
         plt.plot(np.arange(mix_samples.num_time_steps)[:, None].repeat(1000, axis=1), posterior[:, 0, 0].T,
                  color="tab:blue", alpha=0.3)
         plt.plot(np.arange(mix_samples.num_time_steps), mix_samples.values[0, 0, 0], marker="o", color="black",
-                 alpha=0.7)
-        plt.plot(np.arange(mix_samples.num_time_steps), posterior[:, 0, 0].mean(axis=0),
-                 color="tab:pink", alpha=1)
-        plt.title(f"Posterior Predictive")
+                 alpha=0.7, markersize=5)
+        plt.plot(np.arange(mix_samples.num_time_steps), posterior[:, 0, 0].mean(axis=0), color="tab:pink", alpha=1)
+        plt.title(f"Posterior - 1st Feature from 1st Individual")
         plt.show()
 
+        #   Coordination
         posterior = sigmoid(idata.posterior["unbounded_coordination"].sel(chain=0).to_numpy())
 
         plt.figure()
-        plt.plot(np.arange(T)[:, None].repeat(1000, axis=1), posterior.T,
-                 color="tab:blue", alpha=0.3)
-        plt.plot(np.arange(T), coordination_values[0], marker="o", color="black",
-                 alpha=0.7)
-        plt.plot(np.arange(T), posterior.mean(axis=0),
-                 color="tab:pink", alpha=1)
+        plt.plot(np.arange(T)[:, None].repeat(1000, axis=1), posterior.T, color="tab:blue", alpha=0.3)
+        plt.plot(np.arange(T), coordination_values[0], marker="o", color="black", alpha=0.7, markersize=5)
+        plt.plot(np.arange(T), posterior.mean(axis=0), color="tab:pink", alpha=1)
         plt.title(f"Posterior Coordination")
         plt.show()
--- test/mixture_component_test.py
+++ test/mixture_component_test.py
@@ -10,17 +10,13 @@ from coordination.model.components.mixture_component import mixture_logp_with_se
 class TestMixtureComponent(unittest.TestCase):
 
     def test_logp_with_self_dependency(self):
-        # 3 subjects, 2 features and 3 time steps
+        # 3 subjects, 2 features and 2 time steps
         mixture_component = pt.constant(
-            np.array([[[0.1, 0.2, 0.3], [0.2, 0.3, 0.4]], [[0.2, 0.3, 0.4], [0.3, 0.4, 0.5]],
-                      [[0.3, 0.4, 0.5], [0.4, 0.5, 0.6]]]))
+            np.array([[[0.1, 0.3], [0.2, 0.4]], [[0.2, 0.4], [0.3, 0.5]], [[0.3, 0.5], [0.4, 0.6]]]))
         initial_mean = pt.constant(np.array([[0.3, 0.4], [0.4, 0.5], [0.6, 0.8]]))
         sigma = pt.constant(np.array([[0.01, 0.02], [0.02, 0.03], [0.03, 0.04]]))
         mixture_weights = pt.constant(np.array([[0.3, 0.7], [0.6, 0.4], [0.5, 0.5]]))
-        coordination = pt.constant(np.array([0.1, 0.3, 0.7]))
-        prev_time = pt.constant(np.array([-1, -1, 0]))
-        prev_time_mask = pt.constant(np.array([0, 0, 1]))
-        subject_mask = pt.constant(np.array([1, 0, 1]))
+        coordination = pt.constant(np.array([0.1, 0.7]))
 
         expander_aux_mask_matrix = []
         aggregator_aux_mask_matrix = []
@@ -37,10 +33,8 @@ class TestMixtureComponent(unittest.TestCase):
         estimated_logp = mixture_logp_with_self_dependency(mixture_component=mixture_component,
                                                            initial_mean=initial_mean,
                                                            sigma=sigma,
+                                                           mixture_weights=mixture_weights,
                                                            coordination=coordination,
-                                                           prev_time=prev_time,
-                                                           prev_time_mask=prev_time_mask,
-                                                           subject_mask=subject_mask,
                                                            expander_aux_mask_matrix=pt.constant(
                                                                expander_aux_mask_matrix),
                                                            aggregation_aux_mask_matrix=aggregator_aux_mask_matrix)
@@ -49,19 +43,34 @@ class TestMixtureComponent(unittest.TestCase):
         self.assertAlmostEqual(estimated_logp.eval(), real_logp)
 
     def test_logp_without_self_dependency(self):
-        # 3 subjects, 2 features and 3 time steps
+        # 3 subjects, 2 features and 2 time steps
         mixture_component = pt.constant(
-            np.array([[[0.1, 0.2, 0.3], [0.2, 0.3, 0.4]], [[0.2, 0.3, 0.4], [0.3, 0.4, 0.5]],
-                      [[0.3, 0.4, 0.5], [0.4, 0.5, 0.6]]]))
+            np.array([[[0.1, 0.3], [0.2, 0.4]], [[0.2, 0.4], [0.3, 0.5]], [[0.3, 0.5], [0.4, 0.6]]]))
         initial_mean = pt.constant(np.array([[0.3, 0.4], [0.4, 0.5], [0.6, 0.8]]))
         sigma = pt.constant(np.array([[0.01, 0.02], [0.02, 0.03], [0.03, 0.04]]))
         mixture_weights = pt.constant(np.array([[0.3, 0.7], [0.6, 0.4], [0.5, 0.5]]))
-        coordination = pt.constant(np.array([0.1, 0.3, 0.7]))
-        prev_time = pt.constant(np.array([-1, -1, 0]))
-        prev_time_mask = pt.constant(np.array([0, 0, 1]))[None, :]
-        subject_mask = pt.constant(np.array([1, 0, 1]))[None, :]
-        estimated_logp = mixture_logp_without_self_dependency(mixture_component, initial_mean, sigma, mixture_weights,
-                                                              coordination, prev_time, prev_time_mask, subject_mask)
+        coordination = pt.constant(np.array([0.1, 0.7]))
+
+        expander_aux_mask_matrix = []
+        aggregator_aux_mask_matrix = []
+        for subject in range(3):
+            expander_aux_mask_matrix.append(np.delete(np.eye(3), subject, axis=0))
+            aux = np.zeros((3, 2))
+            aux[subject] = 1
+            aux = aux * mixture_weights[subject][None, :]
+            aggregator_aux_mask_matrix.append(aux)
+
+        expander_aux_mask_matrix = np.concatenate(expander_aux_mask_matrix, axis=0)
+        aggregator_aux_mask_matrix = pt.concatenate(aggregator_aux_mask_matrix, axis=1)
+
+        estimated_logp = mixture_logp_without_self_dependency(mixture_component=mixture_component,
+                                                           initial_mean=initial_mean,
+                                                           sigma=sigma,
+                                                           mixture_weights=mixture_weights,
+                                                           coordination=coordination,
+                                                           expander_aux_mask_matrix=pt.constant(
+                                                               expander_aux_mask_matrix),
+                                                           aggregation_aux_mask_matrix=aggregator_aux_mask_matrix)
         real_logp = -4.257365244151745e+02
 
         self.assertAlmostEqual(estimated_logp.eval(), real_logp)
