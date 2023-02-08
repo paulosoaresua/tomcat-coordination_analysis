8efcc364edd66cefc81bf259c98bb81c25a7a219
Paulo Soares <paulosoares@SBS-7186.local>
2023-02-08 16:17:20 -0300

    Link component done

---
+++ coordination/model/components/link_component.py
@@ -0,0 +1,66 @@
+from typing import Any, List, Optional
+
+import numpy as np
+import pymc as pm
+import pytensor.tensor as ptt
+from scipy.stats import bernoulli
+
+from coordination.common.utils import set_random_seed
+from coordination.model.parametrization import Parameter, BetaParameterPrior
+
+
+class LinkComponentParameters:
+
+    def __init__(self, a_p: float, b_p: float):
+        self.p = Parameter(BetaParameterPrior(a_p, b_p))
+
+    def clear_values(self):
+        self.p.value = None
+
+
+class LinkComponentSamples:
+
+    def __init__(self):
+        # For each time step in the component's scale, it contains the time step in the coordination scale
+        # were a link occurred.
+        self.time_steps_in_coordination_scale: List[np.ndarray] = []
+
+    @property
+    def num_time_steps(self):
+        if len(self.time_steps_in_coordination_scale) == 0:
+            return 0
+
+        return self.time_steps_in_coordination_scale[0].shape[-1]
+
+
+class LinkComponent:
+
+    def __init__(self, uuid: str, a_p: float, b_p: float):
+        self.uuid = uuid
+
+        self.parameters = LinkComponentParameters(a_p, b_p)
+
+    def draw_samples(self, num_series: int, seed: Optional[int], coordination: np.ndarray, ) -> LinkComponentSamples:
+        set_random_seed(seed)
+
+        samples = LinkComponentSamples()
+
+        links = bernoulli(p=coordination * self.parameters.p.value).rvs()
+
+        if links.ndim == 1:
+            links = links[None, :]
+
+        for s in range(num_series):
+            samples.time_steps_in_coordination_scale.append(np.array([t for t, l in enumerate(links[s]) if l == 1]))
+
+        return samples
+
+    def update_pymc_model(self, coordination: Any, observation: Any) -> Any:
+        p = pm.Beta(name=f"p_{self.uuid}", alpha=self.parameters.p.prior.a, beta=self.parameters.p.prior.b,
+                    size=1, observed=self.parameters.p.value)
+
+        adjusted_prob = pm.Deterministic(f"adjusted_prob_{self.uuid}", p * coordination)
+
+        pm.Bernoulli(f"link_{self.uuid}", adjusted_prob, observed=observation)
+
+        return p, adjusted_prob
