93318a8f04b5dd0be68cea9e08aa66d78215ff86
Paulo Soares <paulosoares@dhcp-10-142-129-168.uawifi.arizona.edu>
2023-05-18 15:11:38 -0700

    Improving comments in mixture component and removing unused code.

--- coordination/component/mixture_component.py
+++ coordination/component/mixture_component.py
@@ -7,115 +7,113 @@ import pytensor.tensor as ptt
 from scipy.stats import norm
 
 from coordination.common.utils import set_random_seed
-from coordination.model.parametrization import Parameter, HalfNormalParameterPrior, DirichletParameterPrior, \
-    NormalParameterPrior
+from coordination.model.parametrization import Parameter, HalfNormalParameterPrior, NormalParameterPrior
 
 
-def mixture_logp(mixture_component: Any,
+def mixture_logp(sample: Any,
                  initial_mean: Any,
                  sigma: Any,
-                 mixture_weights: Any,
                  coordination: Any,
-                 expander_aux_mask_matrix: ptt.TensorConstant,
                  self_dependent: ptt.TensorConstant):
-    num_subjects = mixture_component.shape[0]
-    num_features = mixture_component.shape[1]
+    """
+    This function computes the log-probability of a mixture component. We use the following definition in the
+    comments below:
 
-    # Log probability due to the initial time step in the component's scale.
-    total_logp = pm.logp(pm.Normal.dist(mu=initial_mean, sigma=sigma, shape=(num_subjects, num_features)),
-                         mixture_component[..., 0]).sum()
+    s: number of subjects
+    d: number of dimensions/features of the component
+    T: number of time steps in the component's scale
+    """
 
-    # D contains the values from other individuals for each individual
-    D = ptt.tensordot(expander_aux_mask_matrix, mixture_component, axes=(1, 0))[..., :-1]  # s * (s-1) x d x t-1
+    N = sample.shape[0]
+    D = sample.shape[1]
 
-    # Current values from each subject. We extend S and point such that they match the dimensions of D.
-    point_extended = ptt.repeat(mixture_component[..., 1:], repeats=(num_subjects - 1), axis=0)
+    # logp at the initial time step
+    total_logp = pm.logp(pm.Normal.dist(mu=initial_mean, sigma=sigma, shape=(N, D)), sample[..., 0]).sum()
+
+    # Contains the sum of previous values of other subjects for each subject scaled by 1/(s-1).
+    # We discard the last value as that is not a previous value of any other.
+    sum_matrix_others = (ptt.ones((N, N)) - ptt.eye(N)) / (N - 1)
+    prev_others = ptt.tensordot(sum_matrix_others, sample, axes=(1, 0))[..., :-1]
 
     if self_dependent.eval():
-        # Previous values from every subject
-        P = mixture_component[..., :-1]  # s x d x t-1
-
-        # Previous values from the same subjects
-        # S_extended = pt.printing.Print("S")(ptt.repeat(P, repeats=(num_subjects - 1), axis=0))
-        S_extended = ptt.repeat(P, repeats=(num_subjects - 1), axis=0)
+        # The component's value for a subject depends on its previous value for the same subject.
+        prev_same = sample[..., :-1]
     else:
-        # Fixed value given by the initial mean for each subject. No self-dependency.
-        S_extended = ptt.repeat(initial_mean[:, :, None], repeats=(num_subjects - 1), axis=0)
+        # The component's value for a subject does not depend on its previous value for the same subject.
+        # At every time step, the value from others is blended with a fixed value given by the component's initial
+        # mean.
+        prev_same = initial_mean[:, :, None]
 
-    # The mask will zero out dependencies on D if we have shifts caused by latent lags. In that case, we cannot infer
-    # coordination if the values do not exist on all the subjects because of gaps introduced by the shift. So we can
-    # only infer the next value of the latent value from its previous one on the same subject,
-    C = coordination[None, None, 1:]  # 1 x 1 x t-1
-    mean = (D - S_extended) * C + S_extended
+    # Coordination does not affect the component in the first time step because the subjects have no previous
+    # dependencies at that time.
+    c = coordination[None, None, 1:]  # 1 x 1 x t-1
 
-    sd = ptt.repeat(sigma, repeats=(num_subjects - 1), axis=0)[:, :, None]
+    blended_mean = (prev_others - prev_same) * c + prev_same
 
-    logp_extended = pm.logp(pm.Normal.dist(mu=mean, sigma=sd, shape=D.shape), point_extended)
-    logp_tmp = logp_extended.reshape((num_subjects, num_subjects - 1, num_features, logp_extended.shape[-1]))
-    total_logp += pm.math.logsumexp(logp_tmp + pm.math.log(mixture_weights[:, :, None, None]), axis=1).sum()
+    # Match the dimensions of the standard deviation with that of the blended mean
+    sd = sigma[:, :, None]
+
+    # Index samples starting from the second index (i = 1) so that we can effectively compare current values against
+    # previous ones (prev_others and prev_same).
+    total_logp += pm.logp(pm.Normal.dist(mu=blended_mean, sigma=sd, shape=blended_mean.shape), sample[..., 1:]).sum()
 
     return total_logp
 
 
 def mixture_random(initial_mean: np.ndarray,
                    sigma: np.ndarray,
-                   mixture_weights: np.ndarray,
                    coordination: np.ndarray,
-                   expander_aux_mask_matrix: np.ndarray,
                    self_dependent: bool,
                    num_subjects: int,
                    dim_value: int,
                    rng: Optional[np.random.Generator] = None,
                    size: Optional[Tuple[int]] = None) -> np.ndarray:
-    num_time_steps = coordination.shape[-1]
+    """
+    This function generates samples from of a mixture component for prior predictive checks. We use the following
+    definition in the comments below:
+
+    s: number of subjects
+    d: number of dimensions/features of the component
+    T: number of time steps in the component's scale
+    """
+
+    T = coordination.shape[-1]
+    N = num_subjects
 
     noise = rng.normal(loc=0, scale=1, size=size) * sigma[:, :, None]
 
-    # We sample the influencers in each time step using the mixture weights
-    influencers = []
-    for subject in range(num_subjects):
-        probs = np.insert(mixture_weights[subject], subject, 0)
-        influencer = rng.choice(a=np.arange(num_subjects), p=probs, size=num_time_steps)
-        # We will use the influencer to index a matrix with 6 columns. One for each pair influencer -> influenced
-        influencers.append(subject * (num_subjects - 1) + np.minimum(influencer, num_subjects - 2))
-    influencers = np.array(influencers)
-
     sample = np.zeros_like(noise)
-    prior_sample = rng.normal(loc=initial_mean, scale=sigma, size=(num_subjects, dim_value))
-    sample[..., 0] = prior_sample
 
-    for t in np.arange(1, num_time_steps):
-        D = np.einsum("ij,jlk->ilk", expander_aux_mask_matrix, sample[..., t - 1][..., None])  # s * (s-1) x d x 1
+    # Sample from prior in the initial time step
+    sample[..., 0] = rng.normal(loc=initial_mean, scale=sigma, size=(num_subjects, dim_value))
 
-        D = D[influencers[..., t]]  # s x d
+    sum_matrix_others = (ptt.ones((N, N)) - ptt.eye(N)) / (N - 1)
+    for t in np.arange(1, T):
+        prev_others = np.dot(sum_matrix_others, sample[..., t - 1])  # s x d
 
-        # Previous sample from the same individual
         if self_dependent:
-            S = sample[..., t - 1]
+            # Previous sample from the same subject
+            prev_same = sample[..., t - 1]
         else:
-            S = initial_mean
+            # No dependency on the same subject. Sample from prior.
+            prev_same = initial_mean
 
-        mean = ((D - S) * coordination[t] + S)
+        blended_mean = (prev_others - prev_same) * coordination[t] + prev_same
 
-        transition_sample = rng.normal(loc=mean, scale=sigma)
-
-        sample[..., t] = transition_sample
+        sample[..., t] = rng.normal(loc=blended_mean, scale=sigma)
 
     return sample + noise
 
 
 class MixtureComponentParameters:
 
-    def __init__(self, mean_mean_a0: np.ndarray, sd_mean_a0: np.ndarray, sd_sd_aa: np.ndarray,
-                 a_mixture_weights: np.ndarray):
+    def __init__(self, mean_mean_a0: np.ndarray, sd_mean_a0: np.ndarray, sd_sd_aa: np.ndarray):
         self.mean_a0 = Parameter(NormalParameterPrior(mean_mean_a0, sd_mean_a0))
         self.sd_aa = Parameter(HalfNormalParameterPrior(sd_sd_aa))
-        self.mixture_weights = Parameter(DirichletParameterPrior(a_mixture_weights))
 
     def clear_values(self):
         self.mean_a0.value = None
         self.sd_aa.value = None
-        self.mixture_weights.value = None
 
 
 class MixtureComponentSamples:
@@ -123,7 +121,7 @@ class MixtureComponentSamples:
     def __init__(self):
         self.values = np.array([])
 
-        # For each time step in the component's scale, it contains the time step in the coordination scale
+        # For each time step in the component's scale, it contains the matching time step in the coordination scale
         self.time_steps_in_coordination_scale = np.array([])
 
     @property
@@ -174,15 +172,13 @@ class MixtureComponent:
 
         self.parameters = MixtureComponentParameters(mean_mean_a0=mean_mean_a0,
                                                      sd_mean_a0=sd_mean_a0,
-                                                     sd_sd_aa=sd_sd_aa,
-                                                     a_mixture_weights=a_mixture_weights)
+                                                     sd_sd_aa=sd_sd_aa)
 
     @property
     def parameter_names(self) -> List[str]:
         names = [
             self.mean_a0_name,
-            self.sd_aa_name,
-            self.mixture_weights_name
+            self.sd_aa_name
         ]
 
         return names
@@ -195,15 +191,12 @@ class MixtureComponent:
     def sd_aa_name(self) -> str:
         return f"sd_aa_{self.uuid}"
 
-    @property
-    def mixture_weights_name(self) -> str:
-        return f"mixture_weights_{self.uuid}"
-
     def clear_parameter_values(self):
         self.parameters.clear_values()
 
-    def draw_samples(self, num_series: int, relative_frequency: float,
-                     coordination: np.ndarray, seed: Optional[int] = None) -> MixtureComponentSamples:
+    def draw_samples(self, relative_frequency: float, coordination: np.ndarray,
+                     seed: Optional[int] = None) -> MixtureComponentSamples:
+
         # Check dimensionality of the parameters
         if self.share_mean_a0_across_features:
             dim_mean_a0_features = 1
@@ -228,23 +221,7 @@ class MixtureComponent:
         assert relative_frequency >= 1
         assert (self.num_subjects, self.num_subjects - 1) == self.parameters.mixture_weights.value.shape
 
-        set_random_seed(seed)
-
-        samples = MixtureComponentSamples()
-
-        num_time_steps_in_cpn_scale = int(coordination.shape[-1] / relative_frequency)
-        samples.time_steps_in_coordination_scale = (np.arange(num_time_steps_in_cpn_scale) * relative_frequency).astype(
-            int)
-
-        # Sample influencers in each time step
-        influencers = []
-        for subject in range(self.num_subjects):
-            probs = np.insert(self.parameters.mixture_weights.value[subject], subject, 0)
-            influencers.append(
-                np.random.choice(a=np.arange(self.num_subjects), p=probs,
-                                 size=(num_series, num_time_steps_in_cpn_scale)))
-        influencers = np.array(influencers).swapaxes(0, 1)
-
+        # Adjust dimensions according to parameter sharing specification
         if self.share_mean_a0_across_subjects:
             mean_a0 = self.parameters.mean_a0.value[None, None, :]
         else:
@@ -255,54 +232,57 @@ class MixtureComponent:
         else:
             sd_aa = self.parameters.sd_aa.value[None, :]
 
-        # draw values from the system dynamics. The default model generates samples by following a Gaussian random walk
+        # Generate samples
+        set_random_seed(seed)
+
+        samples = MixtureComponentSamples()
+
+        num_time_steps_in_cpn_scale = int(coordination.shape[-1] / relative_frequency)
+        samples.time_steps_in_coordination_scale = (np.arange(num_time_steps_in_cpn_scale) * relative_frequency).astype(
+            int)
+
+        # Draw values from the system dynamics. The default model generates samples by following a Gaussian random walk
         # with blended values from different subjects according to the coordination levels over time. Child classes
         # can implement their own dynamics, like spring-mass-damping systems for instance.
         samples.values = self._draw_from_system_dynamics(
             time_steps_in_coordination_scale=samples.time_steps_in_coordination_scale,
             sampled_coordination=coordination,
-            sampled_influencers=influencers,
             mean_a0=mean_a0,
             sd_aa=sd_aa)
 
         return samples
 
     def _draw_from_system_dynamics(self, time_steps_in_coordination_scale: np.ndarray, sampled_coordination: np.ndarray,
-                                   sampled_influencers: np.ndarray, mean_a0: np.ndarray,
-                                   sd_aa: np.ndarray) -> np.ndarray:
+                                   mean_a0: np.ndarray, sd_aa: np.ndarray) -> np.ndarray:
 
         num_series = sampled_coordination.shape[0]
         num_time_steps = len(time_steps_in_coordination_scale)
         values = np.zeros((num_series, self.num_subjects, self.dim_value, num_time_steps))
+
+        N = self.num_subjects
+        sum_matrix_others = (ptt.ones((N, N)) - ptt.eye(N)) / (N - 1)
 
         for t in range(num_time_steps):
             if t == 0:
                 values[..., 0] = norm(loc=mean_a0, scale=sd_aa).rvs(
                     size=(num_series, self.num_subjects, self.dim_value))
             else:
-                C = sampled_coordination[:, time_steps_in_coordination_scale[t]][:, None]
-                P = values[..., t - 1]
+                c = sampled_coordination[:, time_steps_in_coordination_scale[t]][:, None]
 
-                if self.f is not None:
-                    D = self.f(values[..., t - 1], sampled_influencers[..., t])
-                else:
-                    D = P
-
-                D = D[:, sampled_influencers[..., t]][0]
+                prev_others = np.dot(sum_matrix_others, values[..., t - 1])  # s x d
 
                 if self.self_dependent:
-                    S = P
+                    prev_same = values[..., t - 1]
                 else:
-                    S = mean_a0
+                    prev_same = mean_a0
 
-                mean = (D - S) * C + S
+                blended_mean = (prev_others - prev_same) * c + prev_same
 
-                values[..., t] = norm(loc=mean, scale=sd_aa).rvs()
+                values[..., t] = norm(loc=blended_mean, scale=sd_aa).rvs()
 
         return values
 
-    def _create_random_parameters(self, mean_a0: Optional[Any] = None, sd_aa: Optional[Any] = None,
-                                  mixture_weights: Optional[Any] = None):
+    def _create_random_parameters(self, mean_a0: Optional[Any] = None, sd_aa: Optional[Any] = None):
         """
         This function creates the initial mean and standard deviation of the serialized component distribution as
         random variables.
@@ -350,35 +330,17 @@ class MixtureComponent:
                                       size=(self.num_subjects, dim_sd_aa_features),
                                       observed=self.parameters.sd_aa.value)
 
-        if mixture_weights is None:
-            mixture_weights = pm.Dirichlet(name=self.mixture_weights_name,
-                                           a=self.parameters.mixture_weights.prior.a,
-                                           observed=self.parameters.mixture_weights.value)
-
-        return mean_a0, sd_aa, mixture_weights
+        return mean_a0, sd_aa
 
     def update_pymc_model(self, coordination: Any, subject_dimension: str, feature_dimension: str, time_dimension: str,
-                          num_time_steps: int, observed_values: Optional[Any] = None, mean_a0: Optional[Any] = None,
-                          sd_aa: Optional[Any] = None, mixture_weights: Optional[Any] = None) -> Any:
+                          observed_values: Optional[Any] = None, mean_a0: Optional[Any] = None,
+                          sd_aa: Optional[Any] = None) -> Any:
 
-        mean_a0, sd_aa, mixture_weights = self._create_random_parameters(mean_a0, sd_aa, mixture_weights)
-
-        # Auxiliary matrix to compute logp in a vectorized manner without having to loop over the individuals.
-        # The expander matrix transforms a s x f x t tensor to a s * (s-1) x f x t tensor where the rows contain
-        # values of other subjects for each subject in the set.
-        expander_aux_mask_matrix = []
-        for subject in range(self.num_subjects):
-            expander_aux_mask_matrix.append(np.delete(np.eye(self.num_subjects), subject, axis=0))
-            aux = np.zeros((self.num_subjects, self.num_subjects - 1))
-            aux[subject] = 1
-
-        expander_aux_mask_matrix = np.concatenate(expander_aux_mask_matrix, axis=0)
+        mean_a0, sd_aa = self._create_random_parameters(mean_a0, sd_aa)
 
         logp_params = (mean_a0,
                        sd_aa,
-                       mixture_weights,
                        coordination,
-                       expander_aux_mask_matrix,
                        np.array(self.self_dependent),
                        *self._get_extra_logp_params()
                        )
@@ -390,7 +352,7 @@ class MixtureComponent:
                                           dims=[subject_dimension, feature_dimension, time_dimension],
                                           observed=observed_values)
 
-        return mixture_component, mean_a0, sd_aa, mixture_weights
+        return mixture_component, mean_a0, sd_aa
 
     def _get_extra_logp_params(self):
         """
@@ -399,7 +361,13 @@ class MixtureComponent:
         return ()
 
     def _get_logp_fn(self):
+        """
+        Child classes can define their own logp functions
+        """
         return mixture_logp
 
     def _get_random_fn(self):
+        """
+        Child classes can define their own random functions for prior predictive checks
+        """
         return partial(mixture_random, num_subjects=self.num_subjects, dim_value=self.dim_value)
--- test/mixture_component_test.py
+++ test/mixture_component_test.py
@@ -10,66 +10,35 @@ class TestMixtureComponent(unittest.TestCase):
 
     def test_logp_with_self_dependency(self):
         # 3 subjects, 2 features and 2 time steps
-        mixture_component = ptt.constant(
+        sample = ptt.constant(
             np.array([[[0.1, 0.3], [0.2, 0.4]], [[0.2, 0.4], [0.3, 0.5]], [[0.3, 0.5], [0.4, 0.6]]]))
         initial_mean = ptt.constant(np.array([[0.3, 0.4], [0.4, 0.5], [0.6, 0.8]]))
         sigma = ptt.constant(np.array([[0.01, 0.02], [0.02, 0.03], [0.03, 0.04]]))
-        mixture_weights = ptt.constant(np.array([[0.3, 0.7], [0.6, 0.4], [0.5, 0.5]]))
         coordination = ptt.constant(np.array([0.1, 0.7]))
 
-        expander_aux_mask_matrix = []
-        aggregator_aux_mask_matrix = []
-        for subject in range(3):
-            expander_aux_mask_matrix.append(np.delete(np.eye(3), subject, axis=0))
-            aux = np.zeros((3, 2))
-            aux[subject] = 1
-            aux = aux * mixture_weights[subject][None, :]
-            aggregator_aux_mask_matrix.append(aux)
-
-        expander_aux_mask_matrix = np.concatenate(expander_aux_mask_matrix, axis=0)
-        aggregator_aux_mask_matrix = ptt.concatenate(aggregator_aux_mask_matrix, axis=1)
-
-        estimated_logp = mixture_logp(mixture_component=mixture_component,
+        estimated_logp = mixture_logp(sample=sample,
                                       initial_mean=initial_mean,
                                       sigma=sigma,
-                                      mixture_weights=mixture_weights,
                                       coordination=coordination,
-                                      expander_aux_mask_matrix=ptt.constant(
-                                          expander_aux_mask_matrix),
                                       self_dependent=ptt.constant(np.array(True)))
-        real_logp = -5.081544319609303e+02
+        real_logp = -5.973064092657509e+02
 
         self.assertAlmostEqual(estimated_logp.eval(), real_logp)
 
     def test_logp_without_self_dependency(self):
         # 3 subjects, 2 features and 2 time steps
-        mixture_component = ptt.constant(
+        sample = ptt.constant(
             np.array([[[0.1, 0.3], [0.2, 0.4]], [[0.2, 0.4], [0.3, 0.5]], [[0.3, 0.5], [0.4, 0.6]]]))
         initial_mean = ptt.constant(np.array([[0.3, 0.4], [0.4, 0.5], [0.6, 0.8]]))
         sigma = ptt.constant(np.array([[0.01, 0.02], [0.02, 0.03], [0.03, 0.04]]))
         mixture_weights = ptt.constant(np.array([[0.3, 0.7], [0.6, 0.4], [0.5, 0.5]]))
         coordination = ptt.constant(np.array([0.1, 0.7]))
 
-        expander_aux_mask_matrix = []
-        aggregator_aux_mask_matrix = []
-        for subject in range(3):
-            expander_aux_mask_matrix.append(np.delete(np.eye(3), subject, axis=0))
-            aux = np.zeros((3, 2))
-            aux[subject] = 1
-            aux = aux * mixture_weights[subject][None, :]
-            aggregator_aux_mask_matrix.append(aux)
-
-        expander_aux_mask_matrix = np.concatenate(expander_aux_mask_matrix, axis=0)
-        aggregator_aux_mask_matrix = ptt.concatenate(aggregator_aux_mask_matrix, axis=1)
-
-        estimated_logp = mixture_logp(mixture_component=mixture_component,
+        estimated_logp = mixture_logp(sample=sample,
                                       initial_mean=initial_mean,
                                       sigma=sigma,
-                                      mixture_weights=mixture_weights,
                                       coordination=coordination,
-                                      expander_aux_mask_matrix=ptt.constant(
-                                          expander_aux_mask_matrix),
                                       self_dependent=ptt.constant(np.array(False)))
-        real_logp = -4.257365244151745e+02
+        real_logp = -4.673480759324176e+02
 
         self.assertAlmostEqual(estimated_logp.eval(), real_logp)
