bbbd2af46b58cbb051df8291e39a19b6b1748e3f
Paulo Soares <paulosoares@dhcp-10-142-129-168.uawifi.arizona.edu>
2023-05-18 17:33:06 -0700

    Improving comments in coordination model

--- coordination/model/coordination_model.py
+++ coordination/model/coordination_model.py
@@ -5,14 +5,12 @@ from typing import Any, Optional
 import numpy as np
 import xarray
 
-from coordination.common.functions import sigmoid
-
-"""
-Generic classes used by any model of coordination
-"""
-
 
 class CoordinationPosteriorSamples:
+    """
+    This is a generic class that can be used by any model of coordination. It contains methods to generate coordination
+    plots from an inference data object.
+    """
 
     def __init__(self, unbounded_coordination: xarray.Dataset, coordination: xarray.Dataset):
         self.unbounded_coordination = unbounded_coordination
@@ -25,12 +23,21 @@ class CoordinationPosteriorSamples:
 
         return cls(unbounded_coordination, coordination)
 
-    def plot(self, ax: Any, show_samples: bool = False, color: str = "tab:blue", line_width: int = 1,
-             marker_size: Optional[int] = None, label: str = "", samples_color: str = "tab:pink",
+    def plot(self,
+             ax: Any,
+             show_samples: bool = False,
+             color: str = "tab:blue",
+             line_width: int = 1,
+             marker_size: Optional[int] = None,
+             label: str = "",
+             samples_color: str = "tab:pink",
              alpha_band: float = 0.5):
+
         T = self.coordination.sizes["coordination_time"]
         C = self.coordination.sizes["chain"]
         N = self.coordination.sizes["draw"]
+
+        # Use samples from all chains and all draws
         stacked_coordination_samples = self.coordination.stack(chain_plus_draw=("chain", "draw"))
 
         mean_coordination = self.coordination.mean(dim=["chain", "draw"])
@@ -39,15 +46,20 @@ class CoordinationPosteriorSamples:
         upper_band = np.minimum(mean_coordination + sd_coordination, 1)
 
         if show_samples:
+            # Plot all the samples. It can take a while depending on the number of samples used in the inference.
             ax.plot(np.arange(T)[:, None].repeat(N * C, axis=1), stacked_coordination_samples, color=samples_color,
                     alpha=0.3, zorder=1)
+
+        # Show 1 standard-deviation
         ax.fill_between(np.arange(T), lower_band, upper_band, color=color, alpha=alpha_band, zorder=2)
+
         if marker_size is None:
             ax.plot(np.arange(T), mean_coordination, color=color, linewidth=line_width, linestyle="-", zorder=3,
                     label=label)
         else:
             ax.plot(np.arange(T), mean_coordination, color=color, linewidth=line_width, linestyle="-",
                     markersize=marker_size, marker="o", zorder=3, label=label)
+
         ax.set_xlabel("Time Step")
         ax.set_ylabel("Coordination")
         ax.set_xlim([-0.5, T + 0.5])
