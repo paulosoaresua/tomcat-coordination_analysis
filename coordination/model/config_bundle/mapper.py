Fixing issue with the mapper not mapping lists to nparray and normalization with non-array subject indices.

coordination/model/config_bundle/mapper.py
@@ -62,7 +62,7 @@ class DataMapper:
                         #  because observations still keep the feature dimension even if is a
                         #  single one.
                         # Drop the first dimension and keep only the time series.
-                        value = values[0]
+                        value = np.array(values[0])
 
                 else:
                     value = data[mapping["data_column_names"][0]]

coordination/module/observation/serial_gaussian_observation.py
@@ -201,7 +201,7 @@ class SerialGaussianObservation(GaussianObservation):
         normalized_values = np.zeros_like(self.observed_values)
         for subject in range(self.num_subjects):
             # Get observed values for a specific subject across time.
-            idx = (self.subject_indices == subject)
+            idx = (np.array(self.subject_indices) == subject)
             obs_per_subject = self.observed_values[:, idx]
             mean = np.mean(obs_per_subject, axis=-1, keepdims=True)  # mean across time
             std = np.std(obs_per_subject, axis=-1, keepdims=True)