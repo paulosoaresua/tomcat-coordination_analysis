8c9e4efa1b420e0deae3c363c97cf9449772e8f4
Paulo Soares <paulosoares@SBS-7186.local>
2024-01-26 12:15:35 -0700

    Fixing issue with voc+sem model

--- coordination/model/real/vocalic_semantic_link.py
+++ coordination/model/real/vocalic_semantic_link.py
@@ -75,7 +75,7 @@ class VocalicSemanticLinkModel(VocalicModel):
         super()._create_model_from_config_bundle()
         self._model.uuid = "vocalic_semantic_link_model"
 
-        if self.metadata["semantic_link"].time_steps_in_coordination_scale > 0:
+        if len(self.metadata["semantic_link"].time_steps_in_coordination_scale) > 0:
             # We only add the semantic link module if there's evidence.
 
             bundle = self._get_adjusted_bundle()
