350b803691b012332c944391f08d6fc2305751fc
Paulo Soares <paulosoares@SBS-7186.local>
2024-01-30 03:22:39 -0700

    Fixing num ppa error

--- coordination/common/utils.py
+++ coordination/common/utils.py
@@ -30,13 +30,6 @@ class NumpyArrayEncoder(JSONEncoder):
         """
         if isinstance(obj, np.ndarray):
             return obj.tolist()
-        print(obj)
-        if isinstance(obj, np.int64):
-            print(obj)
-            print(obj)
-            print(obj)
-            print(obj)
-            return int(obj)
 
         return JSONEncoder.default(self, obj)
 
